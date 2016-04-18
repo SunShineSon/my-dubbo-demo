b672211612fffacdd4a091d6b4b3fad76d4e13b4
SunShineSon <ggl1314ggl@163.com>
2016-04-18 15:39:34 +0800

    1、dubbo provider

--- dubbo-demo-provider-impl/src/main/java/com/gmail/lan/LuckImpl.java
+++ dubbo-demo-provider-impl/src/main/java/com/gmail/lan/LuckImpl.java
@@ -3,7 +3,7 @@ package com.gmail.lan;
 public class LuckImpl implements Luck{
 
 	public void service() {
-		
+		System.out.println("------dubbo demo provider impl luckimpl service()---------");
 	}
 
 }
